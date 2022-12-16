c314a03fbd04d63603c71d76df5e6761a72336d2
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2022-12-17 00:08:17 +1300

    switch to yarn and ts-node

--- index.ts
+++ index.ts
@@ -1,5 +1,17 @@
 import express from "express";
+import cors from "cors";
+
+require("dotenv").config();
+
 const app = express();
+app.use(express.json());
+app.use(
+  cors({
+    origin: "https://localhost",
+  })
+);
+app.use(express.text());
+
 const port = 3000;
 
 interface Choice {
@@ -24,29 +36,80 @@ interface OpenAICompletionResponse {
   usage: Usage;
 }
 
+interface Message {
+  text: string;
+  party: "bot" | "human";
+  timestamp: number;
+}
+
+function generatePrompt(messages: Message[]) {
+  messages = messages.slice(-25); // sliding window
+
+  let prompt = `Hello, I am a chatbot powered by GPT-3. You can ask me anything and I will try my best to answer your questions.
+
+To format my responses with code blocks, you can use the following markdown syntax:
+
+\`\`\`
+Your code goes here
+\`\`\`
+
+To format my responses with inline code, you can use the following markdown syntax:
+
+\`Your code goes here\`
+
+Feel free to ask me anything and I will do my best to help.
+
+`;
+
+  for (let i = 0; i < messages.length; i++) {
+    const message = messages[i];
+    const { party, text } = message;
+    if (party == "human") {
+      prompt += `Human: ${text.trim()}\n\n`;
+    } else if (party == "bot") {
+      prompt += `Bot: ${text.trim()}\n\n`;
+    }
+  }
+  // prompt for the bot
+  prompt += "Bot: ";
+  return prompt;
+}
+
 app.get("/", async (req, res) => {
-  const BEARER_TOKEN = process.env.BEARER_TOKEN;
-  const model = "text-davinci-002";
-  const temperature = 0.5;
-  const options = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${BEARER_TOKEN}`,
-    },
-    body: JSON.stringify({
-      model,
-      prompt,
-      temperature,
-      max_tokens: 1024,
-    }),
-  };
-  const response = await fetch(
-    "https://api.openai.com/v1/completions",
-    options
-  );
-  const body = (await response.json()) as OpenAICompletionResponse;
-  res.send(body["choices"][0]["text"]);
+  res.contentType("text").send("OK");
+});
+app.post("/generate-chat-completion", async (req, res) => {
+  const messages = req.body.messages as Message[];
+  console.log("messages[messages.length - 1]", messages[messages.length - 1]);
+  try {
+    const BEARER_TOKEN = process.env.BEARER_TOKEN;
+    const model = "text-davinci-002";
+    const temperature = 0.5;
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${BEARER_TOKEN}`,
+      },
+      body: JSON.stringify({
+        model,
+        prompt: generatePrompt(messages),
+        temperature,
+        max_tokens: 1024,
+      }),
+    };
+    const response = await fetch(
+      "https://api.openai.com/v1/completions",
+      options
+    );
+    const body = (await response.json()) as OpenAICompletionResponse;
+    const completion = body["choices"][0]["text"];
+    // send json
+    res.send(JSON.stringify({ success: true, completion }));
+  } catch (error) {
+    console.error(error);
+    res.json({ success: false, error: { message: (error as Error).message } });
+  }
 });
 
 app.listen(port, () => {
