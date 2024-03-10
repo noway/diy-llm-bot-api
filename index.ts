a3540fc49f138ebc717ee773496af385b90e4399
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2024-03-09 21:27:46 -0500

    add claude opus

--- index.ts
+++ index.ts
@@ -185,13 +185,14 @@ app.post("/generate-chat-completion-streaming", async (req, res) => {
     const BEARER_TOKEN = process.env.BEARER_TOKEN;
 
     const isMixtral = model === "mistralai/Mixtral-8x7B-Instruct-v0.1";
+    const isOpus = model === "anthropic/claude-3-opus:beta";
 
-    if (model === "gpt-3.5-turbo" || model === "gpt-4" || model === "gpt-4-1106-preview" || isMixtral) {
+    if (model === "gpt-3.5-turbo" || model === "gpt-4" || model === "gpt-4-1106-preview" || isMixtral || isOpus) {
       if (model === "gpt-4" && authKey !== process.env.AUTH_KEY) {
         throw new Error("Invalid auth key");
       }
       const chatMessages = [
-        ...(!isMixtral ? [{
+        ...(!(isMixtral && isOpus) ? [{
           role: "system",
           content: "You are a helpful AI language model assistant.",
         }] : []),
@@ -204,7 +205,7 @@ app.post("/generate-chat-completion-streaming", async (req, res) => {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          Authorization: `Bearer ${isMixtral ? process.env.DEEPINFRA_BEARER_TOKEN : BEARER_TOKEN}`,
+          Authorization: `Bearer ${isMixtral ? process.env.DEEPINFRA_BEARER_TOKEN : (isOpus ? process.env.OPENROUTER_BEARER_TOKEN : BEARER_TOKEN)}`,
         },
         body: JSON.stringify({
           model,
@@ -214,7 +215,7 @@ app.post("/generate-chat-completion-streaming", async (req, res) => {
         }),
       };
       const response = await fetch(
-        isMixtral ? "https://api.deepinfra.com/v1/openai/chat/completions" : "https://api.openai.com/v1/chat/completions",
+        isMixtral ? "https://api.deepinfra.com/v1/openai/chat/completions" : (isOpus ? "https://openrouter.ai/api/v1/chat/completions" : "https://api.openai.com/v1/chat/completions"),
         options
       );
       if (!response.ok) {
