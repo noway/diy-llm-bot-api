f8b766ae3038b6427d6c6729451a0a4b178492f1
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2025-07-19 22:20:23 +1200

    remove gpt-4.5-preview since it's deprecated

--- index.ts
+++ index.ts
@@ -201,7 +201,6 @@ function getProvider(model: string) {
     case "gpt-3.5-turbo":
     case "gpt-4":
     case "gpt-4-1106-preview":
-    case "gpt-4.5-preview":
     case "gpt-4.1":
     case "gpt-4o-mini":
     case "gpt-4o":
@@ -288,7 +287,7 @@ async function postGenerateChatCompletionStreaming(req: http.IncomingMessage, re
     }
     const { apiType, systemMessage, bearerToken, stop, apiUrl } = modelConfig;
     if (apiType === 'chat') {
-      if ((model === "gpt-4" || model === "gpt-4.5-preview" || model === "gpt-4.1" || model === "o1-preview" || model === "o1-mini") && !timeSafeCompare(authKey ?? "", secrets.AUTH_KEY ?? "")) {
+      if ((model === "gpt-4" || model === "gpt-4.1" || model === "o1-preview" || model === "o1-mini") && !timeSafeCompare(authKey ?? "", secrets.AUTH_KEY ?? "")) {
         throw new Error("Invalid auth key");
       }
       const chatMessages = [
